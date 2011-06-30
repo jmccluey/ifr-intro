16e4e8a4cfab1a2da49ddf3adb2772271551bfb3
mccluejd <mccluejd@5ca5045d-0592-4840-86b5-1fa13a231dc2>
2011-06-30 16:25:02 +0000

    Tagging IFR as a basic PyEPL intro

---
+++ tags/pyepl_intro/exp/ifr/config_ifr.py
@@ -0,0 +1,77 @@
+# Configuration for Immediate Free Recall
+
+"""
+This module sets options for running Immediate Free Recall.
+"""
+
+### experiment structure ###
+nSessions = 2
+
+nLists = 16
+listLength = 24
+wpfile = '../pools/ifr_wordpool.txt'
+
+# word order
+wasfile = '../pools/ifr_was.txt'
+WASthresh = .55 # maximum acceptable WAS similarity between any pair of words
+maxTries = 1200 # maximum number of tries to create a list
+allowPrevSessWords = True
+
+# version file name
+svnVersionFile = 'version.txt'
+
+### stimuli ###
+
+# stimulus display settings
+doMicTest = True
+sessionEndText = "Thank you!\nYou have completed the session."
+recallStartText = '*******'
+wordHeight = .08 # Word Font size (percentage of vertical screen)
+defaultFont = '../fonts/Verdana.ttf'
+fixationHeight = .07
+
+# beep at start and end of recording (freq,dur,rise/fall)
+startBeepFreq = 800
+startBeepDur = 500
+startBeepRiseFall = 100
+stopBeepFreq = 400
+stopBeepDur = 500
+stopBeepRiseFall = 100
+
+# instruction keys
+downButton = 'DOWN'
+upButton = 'UP'
+exitButton = 'RETURN'
+
+# Instructions
+textFiles = dict()
+
+textFiles['introSess'] = 'text/introSess.txt'
+textFiles['introRecall'] = 'text/introRecall.txt'
+textFiles['introQuestions'] = 'text/introQuestions.txt'
+textFiles['introGetReady'] = 'text/introGetReady.txt'
+textFiles['trialBreak'] = 'text/trialBreak.txt'
+
+files = [wpfile,
+         wasfile]
+files.extend(textFiles.values())
+
+### timing ###
+
+# breaks
+instructISI = 500
+preListDelay = 1500
+breakDuration = 5000
+
+# study
+wordDuration = 300
+wordISI = 2000
+jitter = 400
+
+# retention
+preRecallDelay = 1200
+jitterBeforeRecall = 200
+
+# test
+recallDuration = 75000
+
